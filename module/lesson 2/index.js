78dbdeaced38e493f913b053961d3aa63d9aee9a
Saied53 <saydurrahman3952@gmail.com>
2025-11-15 13:12:11 +0600

    file system completed

--- module/lesson 2/index.js
+++ module/lesson 2/index.js
@@ -1,13 +1,65 @@
+const { error } = require("console");
 const fs = require("fs");
 
-fs.writeFile(
-  "demo.txt",
-  "this is simple text using writeFile",
-  function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("successful");
-    }
+// Async Function method
+
+
+// fs.writeFile(
+//   "demo.txt",
+//   "this is simple text using writeFile",
+//   function (error) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("successful");
+//     }
+//   }
+// );
+
+// fs.appendFile(
+//   "demo.txt",
+//   " Now append new text",
+//   function (error) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("successful");
+//     }
+//   }
+// );
+
+// fs.readFile("demo.txt", "utf-8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// fs.rename("demo.txt", "filesytem.txt", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("file rename Suceessful");
+//   }
+// });
+
+
+fs.unlink("filesytem.txt", (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("file delete Suceessful");
   }
-);
+});
+
+// fs.exists("filesytem.txt", (result) => {
+//   if (result) {
+//     console.log("found");
+//   } else {
+//     console.log("not found");
+//   }
+// });
+
+// sync funtion method
+fs.existsSync("filesytem.txt")
