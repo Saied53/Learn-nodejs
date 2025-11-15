4e11fea4b274285c1ff5e9f8d56d0ee00f66cea4
Saied53 <saydurrahman3952@gmail.com>
2025-11-15 21:42:55 +0600

    update3

---
+++ module/lesson 3/index.js
@@ -0,0 +1,16 @@
+//os and path
+
+// const os = require('os')
+// const {freemem, homedir} = require('os')
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+
+// console.log(__dirname);
+
+const path = require('path')
+console.log(path)
+
